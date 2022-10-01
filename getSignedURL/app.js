da475c7bac669c069c62302b8832f6f2113848b2
Skulllady <noorhoque45@gmail.com>
2022-09-30 18:16:33 -0700

    s3 getUploadURL

--- getSignedURL/app.js
+++ getSignedURL/app.js
@@ -20,35 +20,30 @@ AWS.config.update({ region: process.env.AWS_REGION })
 const s3 = new AWS.S3()
 
 // Change this value to adjust the signed URL's expiration
-const URL_EXPIRATION_SECONDS = 300
+const URL_EXPIRATION_SECONDS = 30
 
 // Main Lambda entry point
 exports.handler = async (event) => {
+  console.log("EVENT", event)
   return await getUploadURL(event)
 }
 
-const getUploadURL = async function(event) {
-  const randomID = parseInt(Math.random() * 10000000)
-  const Key = `${randomID}.jpg`
+const getUploadURL = async function (event) {
+  const Key = event.queryStringParameters.game_id
 
   // Get signed URL from S3
   const s3Params = {
     Bucket: process.env.UploadBucket,
     Key,
     Expires: URL_EXPIRATION_SECONDS,
-    ContentType: 'image/jpeg',
-
-    // This ACL makes the uploaded object publicly readable. You must also uncomment
-    // the extra permission for the Lambda function in the SAM template.
-
-    // ACL: 'public-read'
+    ContentType: 'application/json',
+    ACL: 'public-read'
   }
 
   console.log('Params: ', s3Params)
   const uploadURL = await s3.getSignedUrlPromise('putObject', s3Params)
 
   return JSON.stringify({
-    uploadURL: uploadURL,
-    Key
+    uploadURL
   })
 }
